bb5abdede9e6780524faa3f231e3b99b5428cba0
drewcssv <51677490+drewcssv@users.noreply.github.com>
2020-10-13 22:20:04 -0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,8 @@
+
+var markers = [
+        ['BackpAQ 1', 37.875290, -122.541390, '1059654', '/fields/2/','last.json?','api_key=DZ07N6EQQM0GEUPE', '811 Smith Road MV'], 
+        ['BackpAQ 2', 37.875293, -122.541396, '1060405', '/fields/2/','last.json?','api_key=BNY857AMT8UZ8E9X', '811 Smith Road MV'], 
+        ['BackpAQ 3', 37.875293, -122.541399, '1150280', '/fields/2/','last.json?','api_key=BMP7BLV51DNQCHQC', '811 Smith Road MV'], // my sensor
+        ['BackpAQ 4', 37.875293, -122.541399, '1150280', '/fields/2/','last.json?','api_key=CCP02S8KPW8A6BDI', '811 Smith Road MV'],
+        ['BAQ Labs PA', 37.875280, -122.541396, '720433', '/fields/2/','last.json?','api_key=CCP02S8KPW8A6BDI', '811 Smith Rd BackpAQ Labs'] // was SSV 2U
+        ];
