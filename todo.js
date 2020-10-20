805dfc9b1a8a7b0d7f52b5b5d0c92be5ba7c730f
drewcssv <51677490+drewcssv@users.noreply.github.com>
2020-10-20 15:07:56 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -8,3 +8,10 @@ var markers = [
         ];
 
  
+var channelKeys =[];
+        channelKeys.push({channelNumber:1059654, name:'BackpAQ 1',key:'DZ07N6EQQM0GEUPE',
+        fieldList:[{field:1,axis:'O'},{field:2,axis:'O'},{field:3,axis:'O'},{field:4,axis:'O'},{field:5,axis:'O'},{field:6,axis:'T'},{field:7,axis:'H'},{field:8,axis:'O'}]});
+        channelKeys.push({channelNumber:1060405, name:'BackpAQ 2',key:'BNY857AMT8UZ8E9X',
+        fieldList:[{field:1,axis:'O'},{field:2,axis:'O'},{field:3,axis:'O'},{field:4,axis:'O'},{field:5,axis:'O'},{field:6,axis:'T'},{field:7,axis:'H'},{field:8,axis:'O'}]});
+        channelKeys.push({channelNumber:1150280, name:'BackpAQ 3',key:'BMP7BLV51DNQCHQC',
+        fieldList:[{field:2,axis:'D'},{field:4,axis:'S'}]});
